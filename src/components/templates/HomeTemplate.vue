af30cd514fbc2fc308f0e90aced43ac388836483
Dave-Costa <dorivaldo.programador@gmail.com>
2021-09-19 00:09:32 -0300

    fead: add templates

---
+++ src/components/templates/HomeTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+  <Container class="home">
+    <Header />
+    <Presentation />
+    <AllServices />
+  </Container>
+</template>
+<script>
+import { defineComponent } from "vue";
+import { Header, Presentation, AllServices } from "@/components/organisms";
+import { Container } from "@/components/matriz/";
+export default defineComponent({
+  components: {
+    Header,
+    Presentation,
+    AllServices,
+    Container,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.home {
+  margin: 5rem auto;
+  display: grid;
+  grid-gap: 5rem;
+}
+</style>
